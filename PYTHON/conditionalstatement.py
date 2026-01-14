753240780466cfc3fe023438a6405a2643386913
Rishabh Verma <rishabh300verma@gmail.com>
2026-01-14 12:40:12 +0530

    one day or day one

---
+++ PYTHON/conditionalstatement.py
@@ -0,0 +1,34 @@
+'''a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+if(a>b):
+    print("A is greater")
+elif(a==3):
+    print("A is equal to 3")
+else:
+    print("B is greater") '''
+
+
+'''marks=85
+if (marks>=60 and marks<=80):
+    print("pass")
+elif (marks>=80):
+    print("Good")
+else:
+    print("fail") '''
+
+'''age=18
+if age<=18:
+    print("adult")'''
+
+'''age=25
+if age>18 or age<60:
+    print("adult")'''
+
+# x=4
+# if x>5:
+#     if x>8:
+#         print("A")
+#     else:
+#         print("B")
+# else:
+#     print("c")
