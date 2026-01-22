2d557e60facea38231ccc1fbdb04e337a67e7836
Rishabh Verma <rishabh300verma@gmail.com>
2026-01-22 18:58:38 +0530

    one day or day one

--- PYTHON/class1.py
+++ PYTHON/class1.py
@@ -1,14 +1,17 @@
 class dev:
+    raised_amt = 1.40
     def __init__(self, name, last, pay):
         self.name = name
         self.last = last
         self.pay = pay
         self.email = name + last + "@gmail.com"
-    def fullname():
-        return '{} {}'.format(dev_1.name, dev_1.last)    
+    def fullname(self):
+        return '{} {}'.format(self.name, self.last)
+    def inc_pay(self):
+        return int(self.pay * self.raised_amt)
 
 dev_1 = dev("Pritam", "Dey", 150000)
 dev_2 = dev("Jayati", "Mahoto", 15000)
-
-print(dev_1.name)
-print(dev_2.name)
+print(dev_1.pay)
+print(dev_1.fullname())
+print(dev_1.inc_pay())
